1a14aac2c82c397f86a9f5542642368104308976
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2026-06-27 05:30:26 +0900

    Add Express server for clock-in/out functionality and update package dependencies

---
+++ server.js
@@ -0,0 +1,61 @@
+import { exec } from "child_process";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+
+app.post("/clock-in", (req, res) => {
+
+  exec(
+    "npm run clock_in",
+    (error, stdout, stderr) => {
+
+      if (error) {
+        console.error(error);
+
+        return res.status(500).json({
+          success: false,
+          error: error.message
+        });
+      }
+
+      res.json({
+        success: true,
+        output: stdout
+      });
+
+    }
+  );
+
+});
+
+
+app.post("/clock-out", (req, res) => {
+
+  exec(
+    "npm run clock_out",
+    (error, stdout) => {
+
+      if (error) {
+        return res.status(500).json({
+          success:false
+        });
+      }
+
+
+      res.json({
+        success:true,
+        output:stdout
+      });
+
+    }
+  );
+
+});
+
+
+app.listen(3000, () => {
+  console.log("server running :3000");
+});
